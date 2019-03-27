8fe405fd4ac811fbaf700678112acbb914190a70
Matthew Hamilton <somecid183@yahoo.com>
2019-03-27 11:07:42 -0600

    added unown text and did some more mobile stuff

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -11,10 +11,31 @@ const DetailView = ({ pokemon }) => {
       <section className="detail-view">
         <img src={sprite} className='sprite-image' alt="sprite" />
         <div className='data-wrapper' style={{textAlign: "center"}}>
-          <p className ='data-name'>Welcome to the React Pokédex!</p><br />
-          <p className ='data-info'>You can choose a generation by clicking a ball on the left. {id}</p> <br/>
+          <p className='data-name'>Welcome to the React Pokédex!</p><br />
+          <p className='data-info'>You can choose a generation by clicking a ball on the left. {id}</p> <br/>
           <p className='data-info'>Click on a Pokémon to view more details! {type}</p>
         </div>
+      </section>
+    )
+  }
+
+  else if (pokemon.id === 201) {
+    return (
+      <section className="detail-view">
+          <img src={sprite} className='sprite-image' alt="sprite"
+            onMouseOver={e => (e.currentTarget.src = shiny)}
+            onMouseOut={e => (e.currentTarget.src = sprite)} />
+
+        <div className='data-wrapper'>
+          <p className='data-name unown'>name: {name.toLowerCase()}</p>
+          <p className='data-info unown'>pokedex #: {id}</p>
+          <p className='data-info unown'>region: {region.toLowerCase()}</p>
+          <p className='data-info unown'>type: {type.toLowerCase()}</p>
+          <p className='data-info unown'>abilities: {abilities.toLowerCase()}</p>
+          <p className='data-info unown'>height: {height}' {height2}"</p>
+          <p className='data-info unown'>weight: {weight}lbs</p>
+        </div>
+
       </section>
     )
   }
@@ -27,8 +48,8 @@ const DetailView = ({ pokemon }) => {
             onMouseOut={e => (e.currentTarget.src = sprite)} />
 
         <div className='data-wrapper'>
-          <p className ='data-name'>Name: {name}</p>
-          <p className ='data-info'>Pokédex #: {id}</p>
+          <p className='data-name'>Name: {name}</p>
+          <p className='data-info'>Pokédex #: {id}</p>
           <p className='data-info'>Region: {region}</p>
           <p className='data-info'>Type: {type}</p>
           <p className='data-info'>Abilities: {abilities}</p>
